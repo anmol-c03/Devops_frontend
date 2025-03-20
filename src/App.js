a1cf4b350cb3b15d77a6a884d737356f581b4600
Anmol <anmolc650@gmail.com>
2025-03-20 19:16:00 +0545

    changes in api hostname

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ function App() {
         setError(null);
 
         try {
-            const response = await axios.post('http://127.0.0.1:5001/generate', {
+            const response = await axios.post('http://anizalmuseycai/backend:5001/generate', {
                 image: image,
                 // prompt: prompt
             });
